d8c1ef3f8272e1c815efedee30da706901cca070
Maksym Panov <1pan.maxim.52@gmail.com>
2023-07-17 11:42:53 +0300

    Manager side. Added simple navigation bar

--- src/pages/manager/Management.js
+++ src/pages/manager/Management.js
@@ -8,7 +8,8 @@ import {
     Routes,
     Route,
     useLocation, 
-    useNavigate 
+    useNavigate ,
+    Link
 } from "react-router-dom";
 import { 
     ACCESS_DENIED_PAGE, 
@@ -29,6 +30,7 @@ import NewProduct from "../../components/products/NewProduct";
 import ManageDeliveries from "./ManageDeliveries";
 import ManageCategories from "./ManageCategories";
 import ManageUsers from "./ManageUsers";
+import s from "../../style/Management.module.css";
 
 export const Management = () => {
     const user = useSelector(state => state.user);
@@ -70,15 +72,35 @@ export const Management = () => {
     }, []);
 
     return (
-        <Routes>
-            <Route path={ MANAGER_PAGE.substring(8) } element={ <Orders /> } />
-            <Route path={ MANAGE_ORDERS_PAGE.substring(8) } element={ <Orders /> } />
-            <Route path={ MANAGE_PRODUCTS_PAGE.substring(8) } element={ <ManagerProductList /> } />
-            <Route path={ MANAGE_NEW_PRODUCT_PAGE.substring(8) } element={ <NewProduct /> } />
-            <Route path={ MANAGE_DELIVERIES.substring(8) } element={ <ManageDeliveries /> } />
-            <Route path={ MANAGE_CATEGORIES.substring(8) } element={ <ManageCategories /> } />
-            <Route path={ MANAGE_USERS.substring(8) } element={ <ManageUsers /> } />
-            <Route path={ "*" } element={ <Orders /> } />
-        </Routes>
+        <div className={s.mainCont}>
+            <header className={s.nav}>
+                <div className={s.lnkCont}>
+                    <Link to={ MANAGE_ORDERS_PAGE } className={s.lnk}>Orders</Link>
+                </div>
+                <div className={s.lnkCont}>
+                    <Link to={ MANAGE_USERS } className={s.lnk}>Users</Link>
+                </div>
+                <div className={s.lnkCont}>
+                    <Link to={ MANAGE_PRODUCTS_PAGE } className={s.lnk}>Products</Link>
+                </div>
+                <div className={s.lnkCont}>
+                    <Link to={ MANAGE_CATEGORIES } className={s.lnk}>Categories</Link>
+                </div>
+                <div className={s.lnkCont}>
+                    <Link to={ MANAGE_DELIVERIES } className={s.lnk}>Deliveries</Link>
+                </div>
+            </header>
+            <Routes>
+                <Route path={ MANAGER_PAGE.substring(8) } element={ <Orders /> } />
+                <Route path={ MANAGE_ORDERS_PAGE.substring(8) } element={ <Orders /> } />
+                <Route path={ MANAGE_PRODUCTS_PAGE.substring(8) } element={ <ManagerProductList /> } />
+                <Route path={ MANAGE_NEW_PRODUCT_PAGE.substring(8) } element={ <NewProduct /> } />
+                <Route path={ MANAGE_DELIVERIES.substring(8) } element={ <ManageDeliveries /> } />
+                <Route path={ MANAGE_CATEGORIES.substring(8) } element={ <ManageCategories /> } />
+                <Route path={ MANAGE_USERS.substring(8) } element={ <ManageUsers /> } />
+                <Route path={ "*" } element={ <Orders /> } />
+            </Routes>
+        </div>
+
     );
 }
